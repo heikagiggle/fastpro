6a1aae960bfb04f359aeafe8da25fa57f7b08b7b
heikagiggle <okaforemmanuellaoluchi@gmail.com>
2024-11-25 01:50:53 +0100

    fix usesearchparams build issue

--- apps/web/src/app/vendor/dashboard/reports/page.tsx
+++ apps/web/src/app/vendor/dashboard/reports/page.tsx
@@ -1,51 +1,11 @@
-'use client';
+import dynamic from 'next/dynamic';
 
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '@app/components/lib/ui/tabs';
-import SalesAndFinancials from './sales-financials/SalesAndFinancials';
-import RfpReports from './rfp-reports/RfpReports';
-import RfpReportTabs from './RfpReportTabs';
-
+const ReportsPage = dynamic(() => import('./ReportsPage'), { ssr: false });
 const Reports = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const activeTab = searchParams.get('tab') || 'sales-financials';
-
-  const handleTabChange = (newTab: string) => {
-    router.push(`?tab=${newTab}`);
-  };
-
-  useEffect(() => {
-    if (!searchParams.get('tab')) {
-      router.replace(`?tab=sales-financials`);
-    }
-  }, [router, searchParams]);
-
   return (
-    <Tabs value={activeTab} onValueChange={handleTabChange}>
-      <div className=" flex justify-between ">
-        <TabsList className="w-[70%] justify-start border-b border-[#E5E5E5]">
-          <TabsTrigger value="sales-financials">
-            Sales And Financials
-          </TabsTrigger>
-          <TabsTrigger value="rfp-reports">RFP Reports</TabsTrigger>
-        </TabsList>
-
-        <RfpReportTabs />
-      </div>
-      <TabsContent value="sales-financials">
-        <SalesAndFinancials />
-      </TabsContent>
-      <TabsContent value="rfp-reports">
-        <RfpReports />
-      </TabsContent>
-    </Tabs>
+    <div>
+      <ReportsPage />
+    </div>
   );
 };
 
